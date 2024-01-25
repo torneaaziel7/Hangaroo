057d905823dd97ae971716a37cac663a99818dbd
mikeynoodledoo <152453274+mikeynoodledoo@users.noreply.github.com>
2024-01-26 02:46:46 +0800

    Update hangaroo.js

--- hangaroo.js
+++ hangaroo.js
@@ -1,6 +1,33 @@
 <script>
 //store alphabet in an array for checking
 
+
+//load questions
+function newQuestion () {
+// load questions
+// load answer textboxes 
+// reset alphabet buttons to active
+}
+
+
+//load letters to be guessed
+function txtBoxLetter() {
+// create textbox for the letter
+// load letters to be guessed
+}
+
+//load letter options
+function letterOptions() {
+	
+}
+
+//function to call when a letter is chosen
+function chosenLetter (letter) {
+// get chosen button's id
+// set button to inactive
+}
+
+//function to call when player uses a hint
 function hint () {
 // get remaining answer letters and store in an array
 // random number to choose which remaining answer letter is given
@@ -8,30 +35,11 @@ function hint () {
 
 }
 
-function gameLetters (optionsArray, answersArray) {
-
-}
-
-function chooseLetter (letter) {
-// get chosen button's id
-// set button to inactive
-}
-
-function newQuestion () {
-// load questions
-
-// load letters to be guessed
-// load answer textboxes 
-// reset alphabet buttons to active
-}
-
-function txtBoxLetter() {
-// create textbox for the letter
-}	
-
+//main game thread
 function mainGame () {
 // game functions per line of document
-
+// per line, load question (newQuestion), load answer letter textboxes (txtBoxLetter), load alphabet (letterOptions)
+// next line will only run on complete answer or after 3 failed attempts
 // function to pull document 
 
 // function to put question to textbox
