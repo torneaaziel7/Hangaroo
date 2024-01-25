af0dbc392a9b6694f689ad0351ed7579f25d9c53
mikeynoodledoo <152453274+mikeynoodledoo@users.noreply.github.com>
2024-01-26 01:59:01 +0800

    Create hangaroo.js

---
+++ hangaroo.js
@@ -0,0 +1,26 @@
+<script>
+//store alphabet in an array for checking
+
+function hint () {
+// get remaining answer letters and store in an array
+// random number to choose which remaining answer letter is given
+// iterate through which letter was chosen 
+
+}
+
+function gameLetters (optionsArray, answersArray) {
+
+}
+
+function chooseLetter (letter) {
+// get chosen button's id
+// set button to inactive
+}
+
+function newQuestion () {
+// load questions
+// load letters to be guessed
+// reset alphabet buttons to active
+}
+
+</script>
